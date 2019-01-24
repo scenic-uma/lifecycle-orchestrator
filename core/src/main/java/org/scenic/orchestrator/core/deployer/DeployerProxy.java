a827bf60125cab22c2767d2894dc56721dc3772a
Kiuby88 <kiuby88.programming@gmail.com>
2019-01-24 19:29:08 +0100

    Deployer Proxy

--- core/src/main/java/org/scenic/orchestrator/core/deployer/DeployerProxy.java
+++ core/src/main/java/org/scenic/orchestrator/core/deployer/DeployerProxy.java
@@ -1,12 +1,33 @@
 package org.scenic.orchestrator.core.deployer;
 
+import javax.ws.rs.core.Response;
+
+import org.apache.brooklyn.rest.api.ApplicationApi;
+import org.apache.brooklyn.rest.client.BrooklynApi;
+import org.apache.http.HttpStatus;
+import org.scenic.orchestrator.core.exception.DeploymentException;
+import org.springframework.stereotype.Component;
+
 /**
  * Wraps Brooklyn API.
  */
+@Component
 public class DeployerProxy {
 
+    private final BrooklynApi brooklynApi;
+    private final ApplicationApi applicationApi;
 
+    public DeployerProxy(BrooklynApi brooklynApi) {
+        this.brooklynApi = brooklynApi;
+        this.applicationApi = brooklynApi.getApplicationApi();
+    }
 
+    public void deployApp(String appName, String blueprint) {
+        Response a = applicationApi.createFromYaml(blueprint);
+        if (a.getStatus() != HttpStatus.SC_CREATED) {
+            throw new DeploymentException("Error during the deployment of " + appName);
+        }
 
+    }
 
 }
--- core/src/main/java/org/scenic/orchestrator/core/service/AsycnDeploymentOrchestrator.java
+++ core/src/main/java/org/scenic/orchestrator/core/service/AsycnDeploymentOrchestrator.java
@@ -1,6 +1,6 @@
 package org.scenic.orchestrator.core.service;
 
-import org.apache.brooklyn.rest.client.BrooklynApi;
+import org.scenic.orchestrator.core.deployer.DeployerProxy;
 import org.scenic.orchestrator.core.dto.RunningAppContext;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -12,22 +12,19 @@ import org.springframework.stereotype.Component;
 public class AsycnDeploymentOrchestrator implements DeploymentOrchestrator {
 
 
-    private final BrooklynApi brooklynApi;
+    private final DeployerProxy deployerProxy;
 
-    public AsycnDeploymentOrchestrator(BrooklynApi brooklynApi) {
-        this.brooklynApi = brooklynApi;
+    public AsycnDeploymentOrchestrator(DeployerProxy deployerProxy) {
+        this.deployerProxy = deployerProxy;
     }
 
     @Override
     @Async("threadPoolTaskExecutor")
     public void deploy(RunningAppContext runningAppContext) throws InterruptedException {
 
-        System.out.println("===============");
-        Thread.sleep(5000);
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        brooklynApi.getApplicationApi().createFromForm(runningAppContext.getApplicationTopology());
-        System.out.println("************");
+        System.out.println("Start to deploy application: " + runningAppContext.getApplicationName());
+        deployerProxy.deployApp(runningAppContext.getApplicationName(), runningAppContext.getApplicationTopology());
+
     }
 
 
